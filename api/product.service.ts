ab5b4ffff9c3f578999a3fb22b0ed3f9272f66d5
Ali shabani <ali.shabani8331@gmail.com>
2024-12-07 22:32:03 +0330

    create delete product function

--- api/product.service.ts
+++ api/product.service.ts
@@ -1,4 +1,4 @@
-import { IProduct } from "@/types/product.api";
+import { IEditProductRes, IProduct } from "@/types/product.api";
 import { IGlobalRes } from "@/types/global";
 import { client } from "./client";
 import { urls } from "./urls";
@@ -8,10 +8,41 @@ interface IParams {
   limit?: number;
 }
 
-type fetchProductsListType = (_: IParams) => Promise<IGlobalRes<{products : IProduct[]}>>;
+type fetchProductsListType = (
+  _: IParams
+) => Promise<IGlobalRes<{ products: IProduct[] }>>;
 export const fetchProductsList: fetchProductsListType = async (params) => {
   const response = await client.get(urls.product.getAll, {
     params: { page: params?.page || 1, limit: params?.limit || 6 },
   });
   return response.data;
 };
+
+type deleteProductById = (_: string) => Promise<null>;
+export const deleteProductById: deleteProductById = async (id) => {
+  const response = await client.delete(urls.product.delete(id));
+  console.log("delete: ", response);
+  return response.data;
+};
+
+interface editProductBody {
+  category?: string;
+  subcategory?: string;
+  name?: string;
+  price?: number;
+  quantity?: number;
+  brand?: string;
+  description?: string;
+  thumbnail?: string;
+  images?: string[];
+}
+
+// type editProductById = (
+//   id: string,
+//   _: editProductBody
+// ) => Promise<IEditProductRes>;
+// export const editProductById: editProductById = async (id, body) => {
+//   const response = await client.patch(urls.product.edit(id), {body:{name?: body.name, category?: body.category,  }});
+//   console.log("edit: ", response);
+//   return response.data;
+// };
--- types/product.api.ts
+++ types/product.api.ts
@@ -1,10 +1,27 @@
-export interface IProductRes {
+export interface IEditProductRes {
   status: string;
-  page: number;
-  per_page: number;
-  total: number;
-  total_pages: number;
-  data: { products: IProduct[] };
+  data: {
+    product: {
+      rating: {
+        rate: number;
+        count: number;
+      };
+      _id: string;
+      category: string;
+      subcategory: string;
+      name: string;
+      price: number;
+      quantity: number;
+      brand: string;
+      description: string;
+      thumbnail: string;
+      images: string[];
+      createdAt: string;
+      updatedAt: string;
+      slugname: string;
+      __v: number;
+    };
+  };
 }
 
 export interface IProduct {
