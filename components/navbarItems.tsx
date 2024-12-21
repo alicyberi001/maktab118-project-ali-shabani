71ccaf36023a78572ef1d4acc8aaf91ff6eca6c2
Ali shabani <ali.shabani8331@gmail.com>
2024-12-21 14:05:39 +0330

    fox navigation

--- components/navbarItems.tsx
+++ components/navbarItems.tsx
@@ -4,6 +4,7 @@ import useUserStore from "@/lib/zustand/users.store";
 import ProfileDropdown from "./profileDropdown";
 import CartDropdown from "./cartDropdown";
 import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const NavbarItems = () => {
   const { totalUsers } = useUserStore();
@@ -15,10 +16,10 @@ const NavbarItems = () => {
           <CartDropdown />
         </div>
       ) : (
-        <button className="flex gap-1 items-center border border-gray-600 px-3 py-2 text-sm rounded-md font-semibold hover:text-white hover:bg-slate-900">
+        <Link href={"/login"} className="flex gap-1 items-center border border-gray-600 px-3 py-2 text-sm rounded-md font-semibold hover:text-white hover:bg-slate-900">
           <ArrowRightStartOnRectangleIcon className="size-6" />
           <span>ورود | ثبت نام</span>
-        </button>
+        </Link>
       )}
     </>
   );
